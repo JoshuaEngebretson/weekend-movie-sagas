0421d83eeeb85a5e652e8ab95db58f26277b77be
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-11 22:54:43 -0500

    Built MovieDetails page
    - Now calls on dispatch of 'GET_MOVIE_DETAILS' when that page is loaded
    	- the payload is the id that is grabbed from useParams
    - Renders information about the movie that has that id

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,12 +1,37 @@
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom/";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function MovieDetails() {
 
-  const movie = useSelector(store => store.movieDetails)
+  const dispatch = useDispatch();
+  
+  const movie = useSelector(store => store.movieDetails[0])
   console.log('movie inside MovieDetails:', movie);
 
+  let {id} = useParams();
+
+  useEffect(() => {
+    dispatch({
+      type: 'GET_MOVIE_DETAILS',
+      payload: id
+    })
+  }, []);
+
+
   return (
-    <h1>Inside Movie Details page</h1>
+    <>
+      <h1>{movie.title}</h1>
+      <img src={movie.poster} alt={movie.title} />
+      <p>{movie.description}</p>
+      <h2>Associated Genres</h2>
+      <ul>
+        {movie.genres.map(genre => {
+          return <li key={genre}>{genre}</li>
+        })}
+      </ul>
+    </>
   )
 }
 
--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,16 +1,12 @@
-import { useDispatch } from "react-redux";
+
 import { useHistory } from 'react-router-dom/';
 
 function MovieItem({movie}) {
-  const dispatch = useDispatch();
+
   const history = useHistory();
 
   const sendToDetails = () => {
     console.log(`Clicked on ${movie.title}, will send to details`);
-    dispatch({
-      type: 'GET_MOVIE_DETAILS',
-      payload: movie.id
-    })
     history.push(`/details/${movie.id}`)
   }
 
